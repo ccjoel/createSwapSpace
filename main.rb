39446ea5872680e29effb823c8b87cbdb8187811
Joel Quiles <quilesbaker@gmail.com>
2015-09-26 21:45:57 -0400

    checkpoint. need to make swapspace permanent

--- main.rb
+++ main.rb
@@ -36,10 +36,13 @@ end
 # -------------------------------- START SCRIPT --------------------------------
 
 
-totalLinesForSwap = `swapon -s | wc -l`              # swapon shows total swap space allocated
-# If swapon output is just one line, there is not swap space being used
-puts "Current lines on swapon file: #{totalLinesForSwap}"
 
+
+# ---------------- STEP [1] : Check the System for Swap Information ------------
+
+totalLinesForSwap = `swapon -s | wc -l` # swapon shows total swap space allocated
+					# If swapon output is just one line, there is not swap space being used
+puts "Current lines on swapon file: #{totalLinesForSwap}"
 
 if totalLinesForSwap.to_i > 1 or File.directory?("/swapfile")
 	puts "Swap space has already been allocated for this environment"
@@ -48,11 +51,10 @@ end
 
 checkSuccess
 
-# Check the results of the free -m command, to see how much memory there is, plus to double-check swap
+# Check the results of the free -m command,
+# to see how much memory there is, plus to double-check swap
 output = `free -m`
-
 checkSuccess
-
 swapWord = output.split(" ")[17]
 totalSwap = output.split(" ")[18]
 
@@ -63,7 +65,12 @@ end
 
 puts "Total swap: #{totalSwap}"
 
-puts "Let's verify that there is enough space on drive to add swap space"
+
+
+
+#-------- STEP [2] : Check Available Space on the Hard Drive Partition ---------
+
+puts "Let's verify that there is enough space on drive to add swap space."
 
 stat = Sys::Filesystem.stat("/")
 gb_available = stat.block_size * stat.blocks_available / 1024 / 1024 / 1024
@@ -79,22 +86,31 @@ end
 
 checkSuccess
 
+
+
+
+# --------------------- STEP [3] : Create a Swap File --------------------------
+
 # Create file for allocate swap space
 # `fallocate -l 4G /swapfile`
-
 checkSuccess
+
+
+
+
+
+# ------------------- STEP [4] : Enabling the Swap File ------------------------
+
 
 # Add correct rights to swap file
 # `chmod 600 /swapfile`
 
 # Tell system this newly allocated should be of type swap
 #  `mkswap /swapfile`
-
 checkSuccess
 
 # Actually tell environment to use is swap!
 #  `swapon /swapfile`
-
 checkSuccess
 
 # Verify that swapon's new output contains the right swap params
@@ -107,7 +123,7 @@ if totalLinesForSwap.to_i != 1
 	errorExit 4
 end
 
-# TODO: double check free to see that there is actually swap here
+# Double check free to see that there is actually swap here
 output = `free -m`
 
 checkSuccess
@@ -119,7 +135,11 @@ if totalSwap.to_i == 0
 	errorExit 5
 end
 
-# TODO: Make the Swap File Permanent
+
+
+
+
+# ------------------- STEP [5] Make the Swap File Permanent --------------------
 
 # Append the following line:
 # /swapfile   none    swap    sw    0   0
