32077e14f35341327bd56f7e70d8dd1a5d9acf36
Joel Quiles <quilesbaker@gmail.com>
2015-09-26 21:25:03 -0400

    adding swapon test, exit code test, etc to ruby main script

--- main.rb
+++ main.rb
@@ -11,32 +11,43 @@ require 'sys/filesystem'
 
 puts "Testing script"
 
-def checkSuccess()
-  if `echo $?` != 0
-		puts "Error in running last command. Aborting script."
+def checkSuccess
+	result = $?
+  if result.exitstatus != 0
+		puts "Error code #{result.exitstatus} in running last command. Aborting script."
+		puts "Called by #{caller(0)}"
+		exit 100
 	end
 end
 
-puts `echo $LINENO`
+def errorExit(exitCode)
+	return nil # for now
+	return exit exitCode
+end
 
-totalLinesForSwap = `sudo swapon -s | wc -l`              # swapon shows total swap space allocated
+totalLinesForSwap = `swapon -s | wc -l`              # swapon shows total swap space allocated
 # If swapon output is just one line, there is not swap space being used
 puts "Current lines on swapon file: #{totalLinesForSwap}"
 
 
 if totalLinesForSwap.to_i > 1 or File.directory?("/swapfile")
 	puts "Swap space has already been allocated for this environment"
-	#exit 1
+	errorExit 1
 end
+
+checkSuccess
 
 # Check the results of the free -m command, to see how much memory there is, plus to double-check swap
 output = `free -m`
+
+checkSuccess
+
 swapWord = output.split(" ")[17]
 totalSwap = output.split(" ")[18]
 
 if swapWord != "Swap:"
 	puts "Free output in an unexpeted format"
-	#exit 2
+	errorExit 2
 end
 
 puts "Let's verify that there is enough space on drive to add swap space"
@@ -50,32 +61,31 @@ if gb_available > 10
 	puts "Enough disk space available to allocate 4G of swap space"
 else
 	puts "Less than 10G available, won't allocate swap space."
-	#exit 3
+	errorExit 3
 end
 
 checkSuccess
 
-# TODO: allocate swap space
+# Create file for allocate swap space
+# `fallocate -l 4G /swapfile`
 
-# sudo fallocate -l 4G /swapfile
+checkSuccess
 
-# TODO: add correct rights to swap file
+# Add correct rights to swap file
+# `chmod 600 /swapfile`
 
-# sudo chmod 600 /swapfile
+# Tell system this newly allocated should be of type swap
+#  `mkswap /swapfile`
 
-# TODO: tell system this newly allocated should be of type swap
+checkSuccess
 
-# sudo mkswap /swapfile
+# Actually tell environment to use is swap!
+#  `swapon /swapfile`
 
-# TODO: verify exit code of last command by comparing $? to 0
-
-# TODO: Actually tell environment to use is swap!
-
-# sudo swapon /swapfile
+checkSuccess
 
 # TODO: Verify that swapon's new output contains the right swap params
-
-# sudo swapon -s
+#  `swapon -s`
 
 # TODO: double check free to see that there is actually swap here
 
@@ -93,7 +103,7 @@ checkSuccess
 
 # TODO: set swapiness of system
 
-# sudo sysctl vm.swappiness=10
+#  sysctl vm.swappiness=10
 
 # TODO: check that this variable `vm.swappiness` is not set in `/etc/sysctl.conf` file
 
@@ -106,7 +116,7 @@ checkSuccess
 
 # TODO: if cache pressure is not 50, set to 50:
 
-# sudo sysctl vm.vfs_cache_pressure=50
+#  sysctl vm.vfs_cache_pressure=50
 
 # TODO: verify that the pressure is not set in /etc/sysctl.conf
 # vm.vfs_cache_pressure?
